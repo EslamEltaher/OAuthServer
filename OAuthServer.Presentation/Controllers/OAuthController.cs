7f45bed07e45e5d646c21cb8cbfffff5c7fd3a7e
eslam.eltaher <eslam.mohamed@linkdev.com>
2020-02-03 21:24:27 +0200

    added oauth controller added authorize page

---
+++ OAuthServer.Presentation/Controllers/OAuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OAuthServer.Presentation.Models;
+
+namespace OAuthServer.Presentation.Controllers
+{
+    public class OAuthController : Controller
+    {
+        [HttpGet]
+        [Route("OAuth/Authorize")]
+        //public IActionResult AuthorizeLogin(string client_id, string response_type, string redirect_uri, string scope = "", string state = "")
+        public IActionResult AuthorizeLogin([FromQuery]AuthorizeModel model)
+        {
+            bool valid = ModelState.IsValid;
+
+            if(model.response_type != "code")
+            {
+                //ModelState.AddModelError("")
+            }
+
+            return View("Authorize");
+        }
+
+        [HttpPost]
+        [Route("OAuth/Authorize")]
+        public async Task<IActionResult> Authorize()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("OAuth/Token")]
+        public async Task<IActionResult> GetAccessToken()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ OAuthServer.Presentation/Models/AuthorizeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthServer.Presentation.Models
+{
+    public class AuthorizeModel
+    {
+        [Required]
+        public string client_id { get; set; }
+        [Required]
+        public string response_type { get; set; }
+        [Required]
+        public string redirect_uri { get; set; }
+        [Required]
+        public string scope { get; set; }
+        public string state { get; set; }
+    }
+}
