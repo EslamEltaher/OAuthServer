9eca944229e59859bb81b15cf39b4497b34f780d
eslam.eltaher <eslam.mohamed@linkdev.com>
2020-02-04 16:23:32 +0200

    added Util Project & JwtTokenHelper

---
+++ OAuthServer.Util/JwtSecurityTokenHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace OAuthServer.Util
+{
+    public class JwtSecurityTokenHelper
+    {
+        private readonly SecurityKey _securityKey;
+        private readonly JwtTokenConfigurations _config;
+
+        public JwtSecurityTokenHelper(SecurityKey securityKey, JwtTokenConfigurations config)
+        {
+            _securityKey = securityKey;
+            _config = config;
+        }
+
+
+        public string GenerateJwtToken(IEnumerable<Claim> claims)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var signingCredentials = new SigningCredentials(_securityKey, _config.SigningAlgorithm);
+
+            var claimsIdentity = new ClaimsIdentity(claims);
+
+            var tokenDescriptor = new SecurityTokenDescriptor() {
+                IssuedAt = DateTime.Now,
+                NotBefore = DateTime.Now,
+                
+
+                //old params
+                Subject = claimsIdentity,
+                Expires = DateTime.Now.AddSeconds(_config.TokenDuration),
+                SigningCredentials = signingCredentials,
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ OAuthServer.Util/JwtTokenConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Util
+{
+    public class JwtTokenConfigurations
+    {
+        public double TokenDuration { get; set; }
+        public string SigningAlgorithm { get; set; }
+    }
+}
