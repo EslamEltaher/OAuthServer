b8701cc7b9436082789f16db694a9d57bb0178f4
eslam.eltaher <eslam.mohamed@linkdev.com>
2020-02-06 12:45:17 +0200

    allowed multiple authentication schemes for protected resources

---
+++ OAuthServer.Presentation/Controllers/ProtectedResourceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuthServer.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedResourceController : ControllerBase
+    {
+        //resource can be accessed with cookie-based Authentication or JwtBasedAuthentication
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme + "," + CookieAuthenticationDefaults.AuthenticationScheme)]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Data()
+        {
+            return Ok("1,2,3");
+        }
+    }
+}
