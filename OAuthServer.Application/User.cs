491aafe80d4ecda5cdaab89175dccf604373cf7d
eslam.eltaher <eslam.mohamed@linkdev.com>
2020-02-06 16:08:43 +0200

    added application project

---
+++ OAuthServer.Application/User.cs
@@ -0,0 +1,10 @@
+﻿using OAuthServer.Authorization.Models;
+using System;
+
+namespace OAuthServer.Application
+{
+    public class User : IResourceOwner
+    {
+        public string User_Id { get; set; }
+    }
+}
