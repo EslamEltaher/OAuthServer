ac636b2c8ed148aed7ee9eed8a1c3f7b2281b5c6
eslam.eltaher <eslam.mohamed@linkdev.com>
2020-02-06 16:32:31 +0200

    added InitalModel migrations

---
+++ OAuthServer.Persistence/Migrations/20200206143143_InitialModel.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OAuthServer.Persistence.Migrations
+{
+    public partial class InitialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Client_Id = table.Column<string>(nullable: false),
+                    Client_Secret = table.Column<string>(nullable: true),
+                    Redirect_Uri = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Client_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    User_Id = table.Column<string>(nullable: false),
+                    IsDeveloper = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.User_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consents",
+                columns: table => new
+                {
+                    Client_Id = table.Column<string>(nullable: false),
+                    User_Id = table.Column<string>(nullable: false),
+                    Scope = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consents", x => new { x.Client_Id, x.User_Id });
+                    table.ForeignKey(
+                        name: "FK_Consents_Clients_Client_Id",
+                        column: x => x.Client_Id,
+                        principalTable: "Clients",
+                        principalColumn: "Client_Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Consents_Users_User_Id",
+                        column: x => x.User_Id,
+                        principalTable: "Users",
+                        principalColumn: "User_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consents_User_Id",
+                table: "Consents",
+                column: "User_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consents");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
