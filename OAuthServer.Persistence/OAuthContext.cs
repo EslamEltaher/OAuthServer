7b0f0350dc90c743b5155ea2527623750f4e70a2
eslam.eltaher <eslam.mohamed@linkdev.com>
2020-02-06 13:56:42 +0200

    added OAuthServer.Persistence Project

---
+++ OAuthServer.Persistence/OAuthContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OAuthServer.Authorization.EntityFramework;
+using OAuthServer.Authorization.Models;
+using System;
+
+namespace OAuthServer.Persistence
+{
+    public class OAuthContext : DbContext, IAuthorizationContext
+    {
+        //constructor
+        public OAuthContext(DbContextOptions options) : base(options) { }
+
+
+        //IAuthorizationContext
+        public DbSet<Consent> Consents { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
